5600e4fae46c34826239fa4c46724fdb5abd307d
bruce-zxy <bruce_zxy@163.com>
2017-10-20 18:19:59 +0800

    test

--- js/index.js
+++ js/index.js
@@ -2,3 +2,17 @@ console.log($("#widget").blur(function () {
 	console.log($("#widget"));
 	$("#preview div").html($("#widget").text());
 }));
+
+$("#preview .preview_list").dragsort({
+	dragSelector: "div",
+	dragBetween: false,
+	dragEnd: saveOrder,
+	placeHolderTemplate: "<li class='placeHolder'><div></div></li>" 
+});
+
+function saveOrder() {
+	var data = $("preview_list li").map(function() {
+		return $(this).children().html();
+	}).get();
+	$("input[name=list1SortOrder]").val(data.join("|"));
+};
