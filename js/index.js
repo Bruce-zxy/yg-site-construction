c3eb342517a754234f1d1803298b910a1df69dc5
bruce-zxy <bruce_zxy@163.com>
2017-10-22 21:59:57 +0800

    test2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// 展示栏拖拽效果
 console.log($("#widget").blur(function () {
 	console.log($("#widget"));
 	$("#preview div").html($("#widget").text());
@@ -11,8 +12,18 @@ $("#preview .preview_list").dragsort({
 });
 
 function saveOrder() {
-	var data = $("preview_list li").map(function() {
+	var data = $(".preview_list li").map(function() {
 		return $(this).children().html();
 	}).get();
 	$("input[name=list1SortOrder]").val(data.join("|"));
+	console.log(data);
 };
+
+// 编辑栏增加DOM元素
+$('.mod_add').click(function () {
+	$('.preview_list').append($(this).parent().clone());
+	$('.preview_list .mod_add').remove();
+})
+
+
+
