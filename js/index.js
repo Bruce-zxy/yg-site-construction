cf79d3e3d155f3e87a871026ea05091fb2340c88
bruce-zxy <bruce_zxy@163.com>
2017-11-16 09:46:24 +0800

    alter

--- js/index.js
+++ js/index.js
@@ -7,24 +7,24 @@ function HTMLEncode(html) {
     return output;
 }
 // HTML解码
-function HTMLDecode(text) { 
-    var temp = document.createElement("div"); 
-    temp.innerHTML = text; 
-    var output = temp.innerText || temp.textContent; 
-    temp = null; 
-    return output; 
+function HTMLDecode(text) {
+    var temp = document.createElement("div");
+    temp.innerHTML = text;
+    var output = temp.innerText || temp.textContent;
+    temp = null;
+    return output;
 }
 
 // 编辑栏增加DOM元素
-$('.mod_add').click(function () {
-	$('.preview_list').append($(this).parent().clone().append('<span class="delete"></span><span class="edit"></span>'));
-	$('.preview_list .mod_add').remove();
-	$('.preview_list .delete').click(function () {
-		$(this).parent().remove()
-	})
-	$('.preview_list .edit').click(function () {
-		$(this).prev().prev().attr("contenteditable", "true");
-	})
+$('.mod_add').click(function() {
+    $('.preview_list').append($(this).parent().clone().append('<span class="delete"></span><span class="edit"></span>'));
+    $('.preview_list .mod_add').remove();
+    $('.preview_list .delete').click(function() {
+        $(this).parent().remove()
+    })
+    $('.preview_list .edit').click(function() {
+        $(this).prev().prev().attr("contenteditable", "true");
+    })
 })
 
 // 点击mod进行编辑
@@ -34,9 +34,9 @@ $('.mod_add').click(function () {
 
 // 后台发送保存数据
 $('.test').click(function() {
-	var content = $("#preview").clone();
-	content.find('.edit').remove();
-	content.find('.delete').remove();
+    var content = $("#preview").clone();
+    content.find('.edit').remove();
+    content.find('.delete').remove();
     var formatSrc = $.htmlClean(content.html(), {
         format: true,
         allowedAttributes: [
@@ -47,25 +47,22 @@ $('.test').click(function() {
         ]
     });
     console.log(formatSrc);
-	var address = "http://localhost/test.php";
-	var datas = {
-		name: 'mod01',
-		content: formatSrc
-	}
-	$.ajax({
-		type: "post",
-		url: address,
-		data: datas,
-		success: function(data){
-			$('.output').html(data);
-		},
-		error: function(){
-			alert('异步请求失败！');
-		}
-	});
+    var address = "http://localhost/test.php";
+    var datas = {
+        name: 'mod01',
+        content: formatSrc
+    }
+    $.ajax({
+        type: "post",
+        url: address,
+        data: datas,
+        success: function(data) {
+            $('.output').html(data);
+        },
+        error: function() {
+            alert('异步请求失败！');
+        }
+    });
 })
-
-
-
 
 
