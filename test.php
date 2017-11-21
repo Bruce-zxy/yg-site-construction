35f896a3e4fdb18932bd1cd143ff864fa7f24480
bruce-zxy <bruce_zxy@163.com>
2017-11-21 11:50:44 +0800

    Emmm

--- test.php
+++ test.php
@@ -19,12 +19,12 @@ function error($result, $msg)
     $result['error'] = $msg;
     return $result;
 }
-if ($_REQUEST['name'] === null) {
+
+if (empty($_REQUEST['name'])) {
     $result = error($result, "您未输入页面名！");
 } else {
     $result = findContent($mydb, $name);
-
-    if ($result) {
+    if (!empty($result['content'])) {
         if ($result['content'] === $content) {
             $result = error($result, "您未做任何修改！");
         } else {
@@ -32,8 +32,12 @@ if ($_REQUEST['name'] === null) {
             $result = $updateResult ? findContent($mydb, $name) : error($result, '页面更新出错！');
         }
     } else {
-        $addResult = $mydb->add($_REQUEST);
-        $result = $addResult ? findContent($mydb, $name) : error($result, '页面保存出错！');
+        if (empty($_REQUEST["content"])) {
+            $result = error($result, "您上次保存页面未成功，内容为空！");
+        } else {
+            $addResult = $mydb->add($_REQUEST);
+            $result = $addResult ? findContent($mydb, $name) : error($result, '页面保存出错！');
+        }
     }
 }
 
