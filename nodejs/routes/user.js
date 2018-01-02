caf424736fcdaf85b80b0d69296065b5779ff306
bruce-zxy <bruce_zxy@163.com>
2018-01-02 18:36:02 +0800

    01.02

--- nodejs/routes/user.js
+++ nodejs/routes/user.js
@@ -1,78 +1,82 @@
-var mysql = require('mysql');
-
-
-var response;
-var SQL;
-
-function insertSQL() {
-	SQL = 'insert into ' + _TABLE + ' set ';
-}
-function selectSQL() {
-	// body...
-}
-function deleteSQL() {
-	// body...
-}
-function updateSQL() {
-	// body...
-}
-
-function main(req, res) {
-	var _DATABASE = 'test';
-	var _TABLE = 'sites_construction';
-
-	
-
-	//创建连接  
-	var client = mysql.createConnection({
-	    host: 'localhost',
-	    user: 'root',
-	    password: '19950224',
-	    database: _DATABASE,
-	    port: 3306
-	});
+var mysql = require("mysql");
 
 
 
-	switch('type') {
-		case 'insert':
-			insertSQL();
-			break;
-		case 'select':
-			selectSQL();
-			break;
-		case 'delete':
-			deleteSQL();
-			break;
-		case 'update':
-			updateSQL()
-			break;
+/**
+ * [connDB description]
+ * @Author   bruce_zxy
+ * @DateTime 2018-01-02T11:04:26+0800
+ * @param    {String}                 db     数据库名
+ * @param    {String}                 table  数据表名
+ * @param    {String}                 type   操作类型
+ * @param    {Object}                 datas  （可选）包含find,where,data等
+ * @param    {Object}                 others （可选）包含host,user,password,port,sql等
+ * @return   {Object}                        返回获取的数据结果
+ */
+function connDB(db, table, type, datas, others, res) {
+	var _DATABASE = db;
+	var _TABLE = table;
+	var others = others || {};
+	var SQL;
+	var res = res;
+
+	function selectSQL(table, datas) {
+		var find = datas.find || "*";
+		var where = " WHERE " + datas.where || "";
+		return "SELECT " + find + " FROM " + table + where;
 	}
 
-	var insertSQL = 'insert into t_user(name) values("conan"),("fens.me")';
-	var selectSQL = 'select * from t_user limit 10';
-	var deleteSQL = 'delete from t_user';
-	var updateSQL = 'update t_user set name="conan update"  where name="conan"';
+	//创建连接
+	var client = mysql.createConnection({
+	    host: others.host || "localhost",
+	    user: others.user || "root",
+	    password: others.password || "19950224",
+	    database: _DATABASE,
+	    port: others.port || 3306
+	});
+
+	switch(type) {
+		case "insert":
+			SQL = "insert into " + _TABLE + " set ";
+			break;
+		case "select":
+			SQL = selectSQL(_TABLE, datas);
+			break;
+		case "delete":
+			SQL = "insert into " + _TABLE + " set ";
+			break;
+		case "update":
+			SQL = "insert into " + _TABLE + " set ";
+			break;
+		default:
+			SQL = others.sql;
+	};
 
 	client.connect();
 	client.query(
-	    'SELECT content,content_origin FROM ' + _TABLE,
+	    SQL,
 	    function selectCb(err, results, fields) {
 	        if (err) {
 	            throw err;
 	        }
-
 	        if (results) {
-	            response = results;
+	            res.send(results);
 	        }
 	        client.end();
 	    }
 	);
 }
 
-main();
+function connDB() {
+	this.selectSQL = function (table, datas) {
+		var find = datas.find || "*";
+		var where = " WHERE " + datas.where || "";
+		return "SELECT " + find + " FROM " + table + where;
+	}
+}
 
 /* GET users listing. */
 exports.list = function(req, res){
-	res.send(req.body);
+	connDB("test", "files_system", "select", {
+	}, {}, res);
 };
